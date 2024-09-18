d32fdf02fb23870e541eb0671630f9b05f8d87dd
hieunl <hieunl.esolutions@gmail.com>
2024-09-18 10:09:13 +0700

    FIX: bỏ null cho hiển thị dff vật tư

--- ESPlatform.QRCode.IMS.Core/DTOs/KiemKe/Responses/SupplyDffResponse.cs
+++ ESPlatform.QRCode.IMS.Core/DTOs/KiemKe/Responses/SupplyDffResponse.cs
@@ -2,19 +2,19 @@
 
 public class SupplyDffResponse
 {
-    public decimal? SoLuongMatPhamChat { get; set; }
+    public decimal SoLuongMatPhamChat { get; set; } 
 
-    public decimal? PhanTramMatPhamChat { get; set; }
+    public decimal PhanTramMatPhamChat { get; set; } 
 
-    public decimal? SoLuongKemPhamChat { get; set; }
+    public decimal SoLuongKemPhamChat { get; set; }
 
-    public decimal? PhanTramKemPhamChat { get; set; }
+    public decimal PhanTramKemPhamChat { get; set; }
 
-    public decimal? SoLuongDong { get; set; }
+    public decimal SoLuongDong { get; set; }
 
-    public decimal? SoLuongDeNghiThanhLy { get; set; }
+    public decimal SoLuongDeNghiThanhLy { get; set; }
 
-    public decimal? PhanTramDong { get; set; }
+    public decimal PhanTramDong { get; set; }
 
-    public decimal? TsKemPcMatPc { get; set; }
+    public decimal TsKemPcMatPc { get; set; }
 }
